cc4718db8c2b342165cdfd6ea7dbc824cad8b887
Christopher Schmied <schmied@fmp-berlin.de>
2019-09-10 17:06:21 +0200

    Batch Processing now measures and saves the results in the following naming pattern: shortTitle_<frame padded>_<measure type>.csv Measurements are done over all timeframes and over all ROIs

--- src/main/java/de/leibnizfmp/BatchProcessor.java
+++ src/main/java/de/leibnizfmp/BatchProcessor.java
@@ -1,24 +1,23 @@
 package de.leibnizfmp;
 
+import ij.IJ;
 import ij.ImagePlus;
-import ij.gui.Overlay;
 import ij.measure.Calibration;
-import ij.plugin.Commands;
+import ij.measure.ResultsTable;
 import ij.plugin.filter.ParticleAnalyzer;
 import ij.plugin.frame.RoiManager;
 import ij.process.ByteProcessor;
 
-import java.io.File;
 import java.util.ArrayList;
 
-public class BatchProcessor {
+class BatchProcessor {
 
-    String inputDir;
-    String outputDir;
-    ArrayList<String> fileList;
-    Boolean calibrationSetting;
-    Double pxSizeMicron;
-    Double frameRate;
+    private String inputDir;
+    private String outputDir;
+    private ArrayList<String> fileList;
+    private Boolean calibrationSetting;
+    private Double pxSizeMicron;
+    private Double frameRate;
 
     private String projMethod;
     private double sigmaLoG;
@@ -38,9 +37,7 @@ public class BatchProcessor {
     private double maxSizeBack;
     private int stimFrame;
 
-    private Calibration calibration;
-
-    public void loopOverImages() {
+    void loopOverImages() {
 
         int minSizePxSpot;
         int maxSizePxSpot;
@@ -57,11 +54,11 @@ public class BatchProcessor {
 
             if (calibrationSetting) {
 
-                calibration = batchImage.calibrate();
-                minSizePxSpot = batchImage.calculateMinSizePx(pxSizeMicron, minSizeSpot);
+                batchImage.calibrate();
+                minSizePxSpot = Image.calculateMinSizePx(pxSizeMicron, minSizeSpot);
                 maxSizePxSpot = batchImage.calculateMaxSizePx(pxSizeMicron, maxSizeSpot);
 
-                minSizePxBack = batchImage.calculateMinSizePx(pxSizeMicron, minSizeBack);
+                minSizePxBack = Image.calculateMinSizePx(pxSizeMicron, minSizeBack);
                 maxSizePxBack = batchImage.calculateMaxSizePx(pxSizeMicron, maxSizeBack);
 
                 System.out.println("Metadata will be overwritten.");
@@ -70,12 +67,12 @@ public class BatchProcessor {
 
             } else {
 
-                calibration = imageToProcess.getCalibration();
+                Calibration calibration = imageToProcess.getCalibration();
                 Double pxSizeFromImage = calibration.pixelWidth;
-                minSizePxSpot = batchImage.calculateMinSizePx(pxSizeFromImage, minSizeSpot);
+                minSizePxSpot = Image.calculateMinSizePx(pxSizeFromImage, minSizeSpot);
                 maxSizePxSpot = batchImage.calculateMaxSizePx(pxSizeFromImage, maxSizeSpot);
 
-                minSizePxBack = batchImage.calculateMinSizePx(pxSizeFromImage, minSizeBack);
+                minSizePxBack = Image.calculateMinSizePx(pxSizeFromImage, minSizeBack);
                 maxSizePxBack = batchImage.calculateMaxSizePx(pxSizeFromImage, maxSizeBack);
 
                 System.out.println("Metadata will no be overwritten");
@@ -84,13 +81,17 @@ public class BatchProcessor {
 
             spotAnalysis(imageToProcess, minSizePxSpot, maxSizePxSpot);
 
-            //backgroundAnalysis(imageToProcess, minSizePxBack, maxSizePxBack);
+            backgroundAnalysis(imageToProcess, minSizePxBack, maxSizePxBack);
+
+            System.out.println("Measurement in image " + image + " finished");
 
         }
 
+        System.out.println("Finished batch Processing");
+
     }
 
-    public void spotAnalysis(ImagePlus inputImage, int minSizePxSpot, int maxSizePxSpot) {
+    private void spotAnalysis(ImagePlus inputImage, int minSizePxSpot, int maxSizePxSpot) {
 
         DifferenceImage processImage = new DifferenceImage(projMethod);
         ImagePlus diffImage = processImage.createDiffImage(inputImage, stimFrame);
@@ -103,11 +104,43 @@ public class BatchProcessor {
 
         RoiManager manager = new RoiManager();
         ParticleAnalyzer analyzer = new ParticleAnalyzer(2048,0,null, minSizePxSpot, maxSizePxSpot, lowCirc, highCirc );
-        //analyzer.analyze(watershed);
+        analyzer.analyze(watershed);
 
+        // setup measurements
+        ResultsTable table;
+        IJ.run("Set Measurements...", "area mean standard modal min integrated median redirect=None decimal=3");
+
+        // loop over original image
+        int frameNumber = inputImage.getNFrames();
+        System.out.println("There are " + frameNumber + " frames");
+
+        for (int frame = 1; frame <= frameNumber; frame++ ) {
+
+
+            inputImage.setT(frame);
+            System.out.println("Measuring frame " + frame);
+            manager.runCommand(inputImage, "Select All");
+            manager.runCommand(inputImage, "Measure");
+            table = ResultsTable.getResultsTable();
+            table.save(outputDir + inputImage.getShortTitle() + "_" + String.format("%03d", frame) + "_signal.csv");
+
+            if ( IJ.isResultsWindow() ){
+                IJ.selectWindow("Results");
+                IJ.run("Close");
+            }
+
+        }
+
+        if ( IJ.isResultsWindow() ){
+            IJ.selectWindow("Results");
+            IJ.run("Close");
+        }
+
+        manager.close();
+        System.out.println("Measuring intensities in spots finished.");
     }
 
-    public void backgroundAnalysis(ImagePlus inputImage, int minSizePxBack, int maxSizePxBack) {
+    private void backgroundAnalysis(ImagePlus inputImage, int minSizePxBack, int maxSizePxBack) {
 
         BackgroundSegmenter back = new BackgroundSegmenter();
         ByteProcessor background = back.segmentBackground(inputImage, sigmaBackground, thresholdBackground);
@@ -116,11 +149,42 @@ public class BatchProcessor {
         ParticleAnalyzer backAnalyzer = new ParticleAnalyzer(2048,0,null, minSizePxBack, maxSizePxBack);
 
         ImagePlus testBack = new ImagePlus("test", background);
-        //backAnalyzer.analyze(testBack);
+        backAnalyzer.analyze(testBack);
 
+        // setup measurements
+        ResultsTable table;
+        IJ.run("Set Measurements...", "area mean standard modal min integrated median redirect=None decimal=3");
+
+        // loop over original image
+        int frameNumber = inputImage.getNFrames();
+        System.out.println("There are " + frameNumber + " frames");
+
+        for (int frame = 1; frame <= frameNumber; frame++ ) {
+
+            inputImage.setT(frame);
+            System.out.println("Measuring frame " + frame);
+            manager.runCommand(inputImage, "Select All");
+            manager.runCommand(inputImage, "Measure");
+            table = ResultsTable.getResultsTable();
+            table.save(outputDir + inputImage.getShortTitle() + "_" + String.format("%03d", frame) + "_background.csv");
+
+            if ( IJ.isResultsWindow() ){
+                IJ.selectWindow("Results");
+                IJ.run("Close");
+            }
+
+        }
+
+        if ( IJ.isResultsWindow() ){
+            IJ.selectWindow("Results");
+            IJ.run("Close");
+        }
+
+        manager.close();
+        System.out.println("Measuring intensities in background finished.");
     }
 
-    public BatchProcessor(String inputDirectory, String outputDirectory, ArrayList<String> filesToProcess,
+    BatchProcessor(String inputDirectory, String outputDirectory, ArrayList<String> filesToProcess,
                           String projectionMethod, double setSigmaLoG, double setProminence,
                           double setSigmaSpots, double setRollingSpots, String setThresholdSpots, int setRadiusGradient,
                           double setMinSizePxSpot, double setMaxSizePxSpot, double setLowCirc, double setHighCirc,
